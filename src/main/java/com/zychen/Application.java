e16e38279df06d653676852a7e69bf6a2e0dcf56
章源辰 <zhangyuanchen143@163.com>
2017-09-24 23:21:54 +0800

    服务提供者demo

---
+++ src/main/java/com/zychen/Application.java
@@ -0,0 +1,19 @@
+package com.zychen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author 章源辰
+ * @time: 2017/9/24 15:56
+ * @describion:
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        //new SpringApplicationBuilder(Application.class).web(true).run(args);
+        SpringApplication.run(Application.class, args);
+    }
+}
